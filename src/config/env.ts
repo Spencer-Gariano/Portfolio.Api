f731823ab182ee211ca51fad7d1443a21869d8eb
Spencer Gariano <71291359+Spencer-Gariano@users.noreply.github.com>
2026-06-10 14:40:48 -0700

    Updated logged api url

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,10 @@ import 'dotenv/config';
 export const env = {
   NODE_ENV: process.env.NODE_ENV ?? 'development',
   DATABASE_URL: process.env.DATABASE_URL!,
+  API_BASE_URL: process.env.API_BASE_URL!,
+  PORT: process.env.PORT!,
+  LOCAL: process.env.LOCAL ?? 'false',
 };
-
 export const isProduction = env.NODE_ENV === 'production';
 export const isDevelopment = env.NODE_ENV === 'development';
+export const isLocal = isDevelopment && env.LOCAL === 'true';
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import app from './app.js';
+import { env, isLocal } from './config/env.js';
 
-const port = process.env.PORT || 3000;
+const port = env.PORT || 3000;
+const API_URL = isLocal ? `${env.API_BASE_URL}:${port}` : env.API_BASE_URL;
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on ${API_URL}`);
 });
